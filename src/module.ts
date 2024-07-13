31ee8367636a5ffaf3e182ce96d82983e28b82b7
Scott Kennedy <scottyzen@gmail.com>
2024-07-13 01:45:40 +0100

    chore: Update woonuxt-settings plugin version check and add new settings to runtime configuration

--- src/module.ts
+++ src/module.ts
@@ -12,57 +12,74 @@ const WPGraphQLPlugin: Plugin = { id: 'cGx1Z2luOndwLWdyYXBocWwvd3AtZ3JhcGhxbC5wa
 const WooGraphQLPlugin: Plugin = { id: 'cGx1Z2luOndwLWdyYXBocWwtd29vY29tbWVyY2Uvd3AtZ3JhcGhxbC13b29jb21tZXJjZS5waHA', minRequiredVersion: '0.18.2' };
 const WPGraphQLCors: Plugin = { id: 'cGx1Z2luOndwLWdyYXBocWwtY29ycy0yLjEvd3AtZ3JhcGhxbC1jb3JzLnBocA==', minRequiredVersion: '2.1' };
 
-const query = `
-query getWooNuxtSettings {
+const getVersionQuery = `query getVersion {
   woonuxtSettings {
-    primary_color
-    logo
-    publicIntrospectionEnabled
-    frontEndUrl
-    maxPrice
-    productsPerPage
-    currencyCode
-    global_attributes {
-      slug
-      showCount
-      openByDefault
-      label
-      hideEmpty
-    }
-    stripeSettings {
-      enabled
-      testmode
-      test_publishable_key
-      publishable_key
-    }
-    wooNuxtSEO {
-      provider
-      url
-      handle
-    }
+    wooCommerceSettingsVersion
   }
-  pluginWPGraphQL: plugin(id: "${WPGraphQLPlugin.id}")    { name version }
-  pluginWooGraphQL: plugin(id: "${WooGraphQLPlugin.id}")  { name version }
-  pluginWPCORS: plugin(id: "${WPGraphQLCors.id}")         { name version }
-  generalSettings { title }
-}
-`;
+}`;
 
 // Module options TypeScript inteface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: { name: 'woonuxt', configKey: 'woonuxt' },
-  defaults: {},
   async setup(_, nuxt) {
-    // const resolver = createResolver(import.meta.url);
-
     const GQL_HOST = process.env.GQL_HOST ?? null;
+    const LATEST_VERSION = 1056;
+    let WOONUXT_SETTINGS_PLUGIN_VERSION = 0;
 
     if (!GQL_HOST) {
       console.log('\u001B[1;35mGQL_HOST is missing. Make sure you have the GQL_HOST environment variable set.');
       return;
     }
+
+    // Get the version of the woonuxt-settings plugin
+    try {
+      const { data } = await $fetch(GQL_HOST, { method: 'POST', body: JSON.stringify({ query: getVersionQuery }) });
+      const stringVersion = data.woonuxtSettings?.wooCommerceSettingsVersion.replace(/\D/g, '');
+      WOONUXT_SETTINGS_PLUGIN_VERSION = parseFloat(stringVersion);
+      if (WOONUXT_SETTINGS_PLUGIN_VERSION < LATEST_VERSION) {
+        console.log('\u001B[1;35mThere is a new version of woonuxt-settings plugin available. Please update it.');
+      }
+    } catch (error) {}
+
+    const wooNuxtSEO = WOONUXT_SETTINGS_PLUGIN_VERSION > 1043 ? 'wooNuxtSEO { provider url handle }' : '';
+    const currencyCode = WOONUXT_SETTINGS_PLUGIN_VERSION > 1055 ? 'currencyCode' : '';
+    const currencySymbol = WOONUXT_SETTINGS_PLUGIN_VERSION > 1055 ? 'currencySymbol' : '';
+
+    const woonuxtSettings = `{
+        primary_color
+        logo
+        publicIntrospectionEnabled
+        frontEndUrl
+        productsPerPage
+        maxPrice
+        global_attributes {
+          slug
+          showCount
+          openByDefault
+          label
+          hideEmpty
+        }
+        stripeSettings {
+          enabled
+          testmode
+          test_publishable_key
+          publishable_key
+        }
+        ${wooNuxtSEO}
+        ${currencyCode}
+        ${currencySymbol}
+    }`;
+
+    const query = `
+    query getWooNuxtSettings {
+      woonuxtSettings ${woonuxtSettings}
+      pluginWPGraphQL: plugin(id: "${WPGraphQLPlugin.id}")    { name version }
+      pluginWooGraphQL: plugin(id: "${WooGraphQLPlugin.id}")  { name version }
+      pluginWPCORS: plugin(id: "${WPGraphQLCors.id}")         { name version }
+      generalSettings { title }
+    }`;
 
     try {
       const { data } = await $fetch(GQL_HOST, {
@@ -116,6 +133,7 @@ export default defineNuxtModule<ModuleOptions>({
       nuxt.options.runtimeConfig.public.MAX_PRICE = data.woonuxtSettings?.maxPrice || 1000;
       nuxt.options.runtimeConfig.public.FRONT_END_URL = data.woonuxtSettings?.frontEndUrl || null;
       nuxt.options.runtimeConfig.public.CURRENCY_CODE = data.woonuxtSettings?.currencyCode || null;
+      nuxt.options.runtimeConfig.public.CURRENCY_SYMBOL = data.woonuxtSettings?.currencySymbol || null;
       nuxt.options.runtimeConfig.public.WOO_NUXT_SEO = data.woonuxtSettings?.wooNuxtSEO || null;
 
       // Site title
