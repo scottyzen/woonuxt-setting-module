3c5565bdf5a537a87e8f80f6fa45b7f13d8f8f93
Scott Kennedy <scottyzen@gmail.com>
2025-04-07 22:01:54 +0100

    refactor: remove compatibility date and devtools from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2025-04-07",
 });
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,3 @@
 export default defineNuxtConfig({
   modules: ["../src/module"],
-  compatibilityDate: "2025-04-07",
-  devtools: { enabled: true },
 });
